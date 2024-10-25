fbbaa70c89c0de018e2df1fb198f4285f6befe6d
Tim Haboldt <tim.haboldt@planfox.io>
2024-10-25 16:27:04 +0200

    [FIX] Types

--- src/app/patient/[id]/page.tsx
+++ src/app/patient/[id]/page.tsx
@@ -1,11 +1,8 @@
 import PatientSummary from "@/components/patient-summary";
 import { redirect } from "next/navigation";
+import { PageProps } from "../../../../.next/types/app/layout";
 
-export default async function PatientSummaryServer({
-    params,
-}: {
-    params: { id: string };
-}) {
+export default async function PatientSummaryServer({ params }: PageProps) {
     const { id } = await params;
 
     if (id !== "24039") {
